8e928cd911a32847c165bbfb6ea8b01255bd91b1
Daniel Aqil <110409976+dnlaql@users.noreply.github.com>
2025-07-01 11:36:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Use st.cache_data for loading and caching data for better performance
+@st.cache_data
+def load_data():
+    url = 'https://raw.githubusercontent.com/dnlaql/Visualize-threat/refs/heads/main/dataset/updated_edr-threat_with_departments.csv'
+    data = pd.read_csv(url)
+    data['Time Detected'] = pd.to_datetime(data['Time Detected'])
+    return data
+
+df = load_data()
